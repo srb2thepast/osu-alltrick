5342a7764eeed2009734e2fed36c5c988cff97df
srb2thepast <alexhazoume@gmail.com>
2022-10-28 10:39:43 -0400

    Add BeatmapTest.GetContents

--- osuAT.Game.Tests/Types/BeatmapTest.cs
+++ osuAT.Game.Tests/Types/BeatmapTest.cs
@@ -11,13 +11,26 @@ namespace osuAT.Game.Tests.Types
     [TestFixture]
     public class BeatmapTest
     {
+        private Beatmap map;
         [Test]
         public void TestGetContents()
         {
-            Beatmap map = new Beatmap() {
+            AddStep("Create new beatmap",createMap);
+            Assert.Throws<NotSupportedException>("Throws from immediate access",() => {System.Console.WriteLine(Beatmap.Content)});
+            AddStep("Create new beatmap with invalid FolderLocation",() => 
+                {createMap(); map.FolderLocation = ""; }
+            );
+            Assert.Throws<NotSupportedException>("Throws from Loading with invalid Location",() => {map.LoadMapContents()});
+            AddStep("Create new beatmap",createMap);
+            AddStep("Load Content",map.LoadMapContents());
+            AddStep("Access Content", Console.WriteLine(map.Content));
+        }
+
+        private void createMap() {
+            map = new Beatmap() {
                 FolderLocation = "Hi"
-            }
-            Assert.Throws<NotSupportedException>("Throws from immediate access",() => {} ) 
+            };
+            
         }
     }
 }
